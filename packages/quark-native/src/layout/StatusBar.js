19834ad58006dd419b7ae3944297958e4e113ba2
alec aivazis <alec@aivazis.com>
2017-10-07 10:14:16 -0700

    removed errant log

--- packages/quark-native/src/layout/StatusBar.js
+++ packages/quark-native/src/layout/StatusBar.js
@@ -7,11 +7,9 @@ import {
     Platform
 } from 'react-native'
 
-const StatusBar = ({ style, ...unused }) => {
-    console.log(style)
-    // render the native bar
-    return <NativeBar {...{ ...style, ...styles.container }} />
-}
+const StatusBar = ({ style, ...unused }) => (
+    <NativeBar {...{ ...style, ...styles.container }} />
+)
 
 // not using StyleSheet here so we can merge the provided backgroundColor with a default and
 // figure out which to send to the native element
