69c1af9d079199ae09e50458d0c1c582ba06a6f4
alec aivazis <alec@aivazis.com>
2017-10-07 10:23:00 -0700

    removed onPress handler from appBar

--- packages/quark-core/src/components/layout/AppBar.js
+++ packages/quark-core/src/components/layout/AppBar.js
@@ -4,19 +4,9 @@ import { View, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native'
 // local imports
 import { baseDim, grey2 } from 'quark-core/styles'
 
-const AppBar = ({ style, statusBarStyle, onPress, ...unused }) => {
-    // the bar component
-    const view = <View style={[styles.container, style]} {...unused} />
-
-    // if we have a press handler wrap the content in one
-    return onPress ? (
-        <TouchableWithoutFeedback onPress={onPress}>
-            {view}
-        </TouchableWithoutFeedback>
-    ) : (
-        view
-    )
-}
+const AppBar = ({ style, statusBarStyle, onPress, ...unused }) => (
+    <View style={[styles.container, style]} {...unused} />
+)
 
 const styles = StyleSheet.create({
     container: {
