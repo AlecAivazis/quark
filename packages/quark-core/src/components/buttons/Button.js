18e9c7cc4215e2e3e2beb9a226c855d4250240f6
Alec Aivazis <alec@aivazis.com>
2017-12-14 13:36:50 -0800

    updated default button style to match design

--- packages/quark-core/src/components/buttons/Button.js
+++ packages/quark-core/src/components/buttons/Button.js
@@ -6,11 +6,20 @@ import { GetTheme } from 'quark-core'
 import BaseWithText from './Text'
 import type { TextButtonProps } from './Text'
 
-const Button = ({ ...unused }: TextButtonProps) => (
+const Button = ({ style, ...unused }: TextButtonProps) => (
     <GetTheme>
-        {({ white, grey1, grey5 }) => (
-            <BaseWithText defaultColor={white} activeColor={grey1} textColor={grey5} {...unused} />
-        )}
+        {({ white, grey1, grey5 }) => {
+            console.log('blue')
+            return (
+                <BaseWithText
+                    defaultColor={white}
+                    activeColor={grey1}
+                    textColor={grey5}
+                    style={{ ...style, borderColor: grey5, borderWidth: 1 }}
+                    {...unused}
+                />
+            )
+        }}
     </GetTheme>
 )
 
