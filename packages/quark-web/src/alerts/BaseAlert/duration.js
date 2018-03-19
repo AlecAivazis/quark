edf833d4901ac35ee83cd89cf73b4a3869bab8b0
Alec Aivazis <alec@aivazis.com>
2018-03-19 09:58:50 -0700

    changed durations of alert

--- packages/quark-web/src/alerts/BaseAlert/duration.js
+++ packages/quark-web/src/alerts/BaseAlert/duration.js
@@ -1,9 +1,9 @@
 // based on: https://ux.stackexchange.com/questions/11203/how-long-should-a-temporary-notification-toast-appear
 
 // min time to display alert
-export const min = 3500
+export const min = 3000
 // max time to display alert
-export const max = 7500
+export const max = 10000
 // multiplier
 export const perCharTime = 50
 
