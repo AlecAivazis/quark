5a08f4648a6a7fcc9663d84b0ebf0d2e80766dd1
meinstein <max@datascience.com>
2018-04-09 23:42:35 -0700

    fix tests

--- packages/quark-core/src/components/layout/FlexColumn/index.js
+++ packages/quark-core/src/components/layout/FlexColumn/index.js
@@ -2,7 +2,7 @@
 // external imports
 import React from 'react'
 //local imports
-import { Flex } from './Flex'
+import { Flex } from '../Flex'
 import type { FlexViewPropTypes } from '../Flex'
 
 const FlexColumn = ({ ...unused }: FlexViewPropTypes) => <Flex direction="column" {...unused} />
--- packages/quark-core/src/components/layout/FlexRow/index.js
+++ packages/quark-core/src/components/layout/FlexRow/index.js
@@ -2,7 +2,7 @@
 // external imports
 import React from 'react'
 //local imports
-import { Flex } from './Flex'
+import { Flex } from '../Flex'
 import type { FlexViewPropTypes } from '../Flex'
 
 const FlexRow = ({ ...unused }: FlexViewPropTypes) => <Flex direction="row" {...unused} />
